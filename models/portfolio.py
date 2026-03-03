fe630b68b1cee7c0e9460081f09737fcadf3a248
Can Sever <cansever1005@gmail.com>
2026-03-03 13:25:52 +0300

    added buy_asset method to the portfolio class

--- models/portfolio.py
+++ models/portfolio.py
@@ -6,4 +6,13 @@ class Portfolio:
 
     def add_asset(self, asset: Asset):
         self.assets[asset.aid] = asset
-        self.transactions[asset.aid] = f"Added asset {asset.aid} with quantity {asset.quantity} at average cost {asset.avg_cost}$."
+        self.transactions[asset.aid] = f"Added asset {asset.aid} with quantity {asset.quantity} at price {asset.asset_price}$."
+        
+
+    def buy_asset(self, asset: Asset, quantity: int):
+        if asset.aid in self.assets:
+            self.assets[asset.aid].quantity += quantity
+            self.transactions[asset.aid] = f"Bought {quantity} of {asset.aid} at price {asset.asset_price}$."
+        else:
+            print(f"Asset {asset.aid} not found in portfolio. Please add it first.")
+        
