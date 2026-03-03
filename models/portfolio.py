ad859adfe58dc15e64c494a9e8cf86af1314a350
Can Sever <cansever1005@gmail.com>
2026-03-03 13:30:56 +0300

    changed transaction log

--- models/portfolio.py
+++ models/portfolio.py
@@ -2,17 +2,17 @@ class Portfolio:
     def __init__(self, p_name: str):
         self.name = p_name
         self.assets = {}
-        self.transactions = {}
+        self.transactions = []
 
     def add_asset(self, asset: Asset):
         self.assets[asset.aid] = asset
-        self.transactions[asset.aid] = f"Added asset {asset.aid} with quantity {asset.quantity} at price {asset.asset_price}$."
+        self.transactions.append(f"Added asset {asset.aid} with quantity {asset.quantity} at price {asset.asset_price}$.")
         
 
     def buy_asset(self, asset: Asset, quantity: int):
         if asset.aid in self.assets:
             self.assets[asset.aid].quantity += quantity
-            self.transactions[asset.aid] = f"Bought {quantity} of {asset.aid} at price {asset.asset_price}$."
+            self.transactions.append(f"Bought {quantity} of {asset.aid} at price {asset.asset_price}$.")
         else:
             print(f"Asset {asset.aid} not found in portfolio. Please add it first.")
         
