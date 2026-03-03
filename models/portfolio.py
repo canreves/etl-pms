6b27f02cabe8cb42585862e43d7cc60f7fb1f71f
Can Sever <cansever1005@gmail.com>
2026-03-03 13:12:55 +0300

    changed add_asset method

--- models/portfolio.py
+++ models/portfolio.py
@@ -2,8 +2,8 @@ class Portfolio:
     def __init__(self, p_name: str):
         self.name = p_name
         self.assets = {}
-        self.transactions = []
+        self.transactions = {}
 
     def add_asset(self, asset: Asset):
-        self.assets[asset.aid] = f"{asset.quantity} at {asset.avg_cost}$"
-        self.transactions.append(f"Added asset {asset.aid} with quantity {asset.quantity} at average cost {asset.avg_cost}$.")
+        self.assets[asset.aid] = asset
+        self.transactions[asset.aid] = f"Added asset {asset.aid} with quantity {asset.quantity} at average cost {asset.avg_cost}$."
