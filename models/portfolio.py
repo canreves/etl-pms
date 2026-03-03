e153f49f8b48de00d7d5b88fa0c26e94545d8103
Can Sever <cansever1005@gmail.com>
2026-03-03 13:10:12 +0300

    refinements at portfolio class

--- models/portfolio.py
+++ models/portfolio.py
@@ -1,4 +1,9 @@
 class Portfolio:
-    def __init__(self):
+    def __init__(self, p_name: str):
+        self.name = p_name
         self.assets = {}
         self.transactions = []
+
+    def add_asset(self, asset: Asset):
+        self.assets[asset.aid] = f"{asset.quantity} at {asset.avg_cost}$"
+        self.transactions.append(f"Added asset {asset.aid} with quantity {asset.quantity} at average cost {asset.avg_cost}$.")
